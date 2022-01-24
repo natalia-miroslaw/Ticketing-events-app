49c6f9dfddce67a711440138b49dfa0177de4d8a
natalia <n.miroslaw93@gmail.com>
2022-01-24 16:14:04 +0100

    feat(app): add project files and config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,13 +14,10 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
-
-export default App;
+};
