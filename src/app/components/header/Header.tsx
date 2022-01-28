48870ee3d1355045aafef694d4b91465762c6713
natalia <n.miroslaw93@gmail.com>
2022-01-28 11:29:21 +0100

    feat(header): add LogInButton, Menu and MenuButton containers to Header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import logo from '../../../assets/icons/logo.png';
+import { LogInButton } from './LogInButton';
+import { Menu } from './Menu';
 
 const StyledHeader = styled.header`
   width: 100%;
   padding: 2%;
   border-bottom: 1px solid black;
+  display: inline-flex;
+`;
+
+const LogoTag = styled.img`
+  width: 175px;
 `;
 
 export const Header: React.FC = () => {
   return (
     <StyledHeader>
-      <div>Logo</div>
-      <div>Register button</div>
-      <div>Log in button</div>
-      <div>Menu component</div>
+      <a href="http://localhost:3000/">
+        <LogoTag src={logo} alt="Home Page" />
+      </a>
+      <LogInButton>Sign in</LogInButton>
+      <LogInButton>Log in</LogInButton>
+      <Menu>Menu component</Menu>
     </StyledHeader>
   );
 };
