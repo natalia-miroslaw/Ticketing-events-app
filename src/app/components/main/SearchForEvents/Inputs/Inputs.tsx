3d6b9582ac563657680b2a53da227badf2afb6cc
natalia <n.miroslaw93@gmail.com>
2022-03-10 11:15:54 +0100

    feat(work-in-progress): add redux

--- src/app/components/main/SearchForEvents/Inputs/Inputs.tsx
+++ src/app/components/main/SearchForEvents/Inputs/Inputs.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const InputsContainer = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 5px;
-  border: 1px solid black;
-`;
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export const Inputs: React.FC = () => {
-  const inputTextChangeHandler = () => {};
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
 
   return (
-    <InputsContainer>
-      <label>What are you interested in?</label>
-      <input type="text" onChange={inputTextChangeHandler} />
-      <label>Date</label>
-      <input type="date" />
-      <button type="submit">Search</button>
-    </InputsContainer>
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
   );
 };
+
+// import React from 'react';
+// import styled from 'styled-components';
+
+// const InputsContainer = styled.div`
+//   width: 50%;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   margin: 5px;
+//   border: 1px solid black;
+// `;
+
+// export const Inputs: React.FC = () => {
+//   const inputTextChangeHandler = () => {};
+
+//   return (
+//     <InputsContainer>
+//       <label>What are you interested in?</label>
+//       <input type="text" onChange={inputTextChangeHandler} />
+//       <label>Date</label>
+//       <input type="date" />
+//       <button type="submit">Search</button>
+//     </InputsContainer>
+//   );
+// };
